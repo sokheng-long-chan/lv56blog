79cc5cb9dca270bf71f327dce0abcff2d32cfa2c
sokheng-long-chan <sokhenglongchan.rupp.edu@hotmail.com>
2018-04-19 09:24:19 +0700

    updated code

--- public/js/app.js
+++ public/js/app.js
@@ -47343,14 +47343,10 @@ var staticRenderFns = [
       _c("div", { staticClass: "row justify-content-center" }, [
         _c("div", { staticClass: "col-md-8" }, [
           _c("div", { staticClass: "card" }, [
-            _c("div", { staticClass: "card-header" }, [
-              _vm._v("Example Component")
-            ]),
+            _c("div", { staticClass: "card-header" }, [_vm._v("Card Header")]),
             _vm._v(" "),
             _c("div", { staticClass: "card-body" }, [
-              _vm._v(
-                "\n                    I'm an example component.\n                "
-              )
+              _vm._v("\n                    Card Body.\n                ")
             ])
           ])
         ])
--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -3,10 +3,10 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Example Component</div>
+                    <div class="card-header">Card Header</div>
 
                     <div class="card-body">
-                        I'm an example component.
+                        Card Body.
                     </div>
                 </div>
             </div>
